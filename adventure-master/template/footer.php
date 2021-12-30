a6113e3cbf1e4eab65f4ecb2264aff583e530b97
Richard Tandy Japutra <richardtandy2015@gmail.com>
2021-12-30 21:09:46 +0700

    tambah type

--- adventure-master/template/footer.php
+++ adventure-master/template/footer.php
@@ -36,6 +36,11 @@
                 </div>
             </div>
 
+            <script src="../js/jquery-3.3.1.min.js"></script>
+            <script src="../js/popper.min.js"></script>
+            <script src="../js/bootstrap.min.js"></script>
+            <script src="../js/main.js"></script>
+
             <!-- Bootstrap core JavaScript-->
             <script src="../vendor/jquery/jquery.min.js"></script>
             <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -52,6 +57,11 @@
             <!-- Page level custom scripts -->
             <script src="../js/demo/chart-area-demo.js"></script>
             <script src="../js/demo/chart-pie-demo.js"></script>
+
+            <!-- Page level plugins -->
+            <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+            <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
 
             </body>
 
--- conn.php
+++ conn.php
@@ -2,6 +2,6 @@
 // Richard
 // DESKTOP-65NA9HQ
 $serverName = "DESKTOP-65NA9HQ"; //serverName\instanceName
-$connectionInfo = ["Database"=>"FINPRO"]; //nama db
+$connectionInfo = ["Database"=>"travel"]; //nama db
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 ?>
