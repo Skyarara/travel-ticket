e08b8ac4db316ace477aa950d88021dfba749344
EzraG21 <ezragabrielm@gmail.com>
2021-12-30 21:13:22 +0700

    test

--- conn.php
+++ conn.php
@@ -1,6 +1,8 @@
 <?php
 // Richard
 // DESKTOP-65NA9HQ
+// Ezra
+// LAPTOP-TAG7N02B
 $serverName = "DESKTOP-65NA9HQ"; //serverName\instanceName
 $connectionInfo = ["Database"=>"FINPRO"]; //nama db
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
