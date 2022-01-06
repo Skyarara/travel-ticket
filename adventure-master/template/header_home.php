7989a057276342f31e38683a710ec36891cd1455
Khawarga <gregoriousjuan@gmail.com>
2022-01-06 12:27:21 +0700

    benerin navbar

--- adventure-master/template/header_home.php
+++ adventure-master/template/header_home.php
@@ -44,7 +44,7 @@
 
 							  <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
 							    <ul class="navbar-nav">
-									<li><a href="#home">Home</a></li>
+									<li><a href="index.php">Home</a></li>
 									<li><a href="#about">About</a></li>									
 									<li><a href="katalog.php">Katalog</a></li>
 									<li><a href="#">Transaksi</a></li>
